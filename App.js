9cb60a30c97ab5048e630a7faaf468c2d0281541
khoshi-higashi <89444685+khoshi-higashi@users.noreply.github.com>
2022-03-27 18:35:55 +0900

    Add dummy text

--- App.js
+++ App.js
@@ -5,6 +5,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
